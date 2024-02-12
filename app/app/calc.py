3f280c5ebcf201b3185a94a789d881c82d345883
Morgan J <40442819+MorganEJLA@users.noreply.github.com>
2024-02-11 19:40:03 -0500

    hey

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+    Calcuations
+    
+    This module contains the basic calculations"""
+    
+def add(x, y):
+    """ add x and y and return result"""
+    return x + y
+
+def subtract(x, y):
+    """ subtract y from x and return result"""
+    return y - x
